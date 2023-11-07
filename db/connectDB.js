69a527cf3b8add7b3ac9af07754e9d0dc80c688c
firstmentor <jainvikas887@gmail.com>
2023-11-07 10:45:59 +0530

    ffhh

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const url = "mongodb://0.0.0.0:27017/AdmissionPortalP"
 //mongo db cluster url
-const liveURL = 'mongodb+srv://yshrivastava1404:yash14@cluster0.0rn96sv.mongodb.net/?retryWrites=true&w=majority'
+const liveURL = 'mongodb+srv://pn234:pn12345@cluster0.0zdmk.mongodb.net/admissionportal?retryWrites=true&w=majority'
 const connectDB = ()=>{
     return mongoose.connect(liveURL)
     .then(()=>{
